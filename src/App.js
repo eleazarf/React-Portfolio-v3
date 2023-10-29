c753eb16bd785ef827b5d16fb628b532b3f12e1e
EleazarF <eleazar.fuentes@gmail.com>
2023-10-29 16:30:25 -0600

    Cleaning code from import errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header/Header";
+import Header from "./components/Header/index";
+import Home from "./pages/Home/index";
+import About from "./pages/About/index";
+import Projects from "./pages/Projects/index";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
       </Router>
     </div>
   );
