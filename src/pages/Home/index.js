55963d99b59a2a0dde4d1135cbc1cff4f0be686f
EleazarF <eleazar.fuentes@gmail.com>
2023-10-29 13:15:03 -0600

    added header and home page queries
    added header and home page queries

---
+++ src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAt } from "@fortawesome/free-solid-svg-icons";
+
+function Home() {
+  return (
+    <section className="banner">
+      <div className="banner-content">
+        <p className="greeting">Hi there!</p>
+        <h1>
+          I'm Eleazar{" "}
+        </h1>
+        <p className="job">A Full Stack Web Developer Enthusiast</p>
+        <a href="mailto:eleazar.fuentes@gmail.com" className="contact-btn">
+          <FontAwesomeIcon icon={faAt} /> Contact
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
