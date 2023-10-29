1668d1d5e1a1536c8c06423ccd1473c06e0ff85d
EleazarF <eleazar.fuentes@gmail.com>
2023-10-29 13:34:43 -0600

    Added content to about page

---
+++ src/pages/About/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./about.css";
+import myImage from "/Users/Eleazar/Downloads/React-Portfolio/src/assets/images/eleazarf.png";
+
+
+function About() {
+  return (
+    <>
+      <section className="about-container">
+        <div className="about-content">
+          <h2 className="about-heading">About Me</h2>
+          <p>
+          I'm an Electronics and Communications Engineer with 15+ years of expertise in IT security solutions. I've excelled in consulting, product development, and teaching information security. Additionally, I've explored web development, learning HTML, CSS, JavaScript, jQuery, Bootstrap, and more. My passion lies in technology-driven solutions and fostering innovation.{" "}
+          </p>
+        </div>
+        <img src={myImage} className="my-image" alt="Eleazar Fuentes" />
+      </section>
+    </>
+  );
+}
+
+export default About;
